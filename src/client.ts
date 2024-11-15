2081169b7b111a3865ca9f7e73388393b9a0fbe0
Himanshujadhav2004 <109407236+Himanshujadhav2004@users.noreply.github.com>
2024-11-15 15:46:20 +0530

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { createThirdwebClient } from "thirdweb";
 
 // Environment variable usage
-const clientId = "908f6111213ffd80a2b038b3c24df28e";
+const clientId =import.meta.env.VITE_TEMPLATE_CLIENT_ID;
 
 export const client = createThirdwebClient({
   clientId: clientId,
